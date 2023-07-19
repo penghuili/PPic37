63113cfbec00b84f3a34b93fb9091bc7b3188966
Peng <1a84@proton.me>
2023-07-19 13:08:03 +0200

    init

---
+++ src/store/reducers.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux';
+
+import { sharedReducer } from '../shared/react/store/sharedReducer';
+
+export const reducers = combineReducers({
+  shared: sharedReducer,
+});
